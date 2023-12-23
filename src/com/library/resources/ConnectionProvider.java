629f4afb4e75aac5b5fe685d20736b5c446115e6
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2023-12-23 13:51:47 +0530

    Established connection

---
+++ src/com/library/resources/ConnectionProvider.java
@@ -0,0 +1,21 @@
+package com.library.resources;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+    private Connection conn;
+    public Connection createConnection(){
+        try{
+            //Load the class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_manage","root","Kuldeep@123");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
